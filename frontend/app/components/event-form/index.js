fe71d35f921f964297092dd8657d7c429a7eb0b1
Nick Lontorfos <nick@touchtribe.nl>
2020-02-10 22:12:21 +0100

    refactored formik

--- frontend/app/components/event-form/index.js
+++ frontend/app/components/event-form/index.js
@@ -14,120 +14,135 @@ class EventForm extends Component {
   static defaultProps = {
     events: []
   }
-    onSubmit = (props) => {
-      console.log('props: ', props)
-    }
-    render () {
-      const { events } = this.props
-      const todaysDate = new Date().toISOString().split('T')[0]
 
-      const initialValues = {
-        firstName: '',
-        lastName: '',
-        email: '',
-        dateOfBirth: '', // TODO this needs to be a timestamp
-        gender: '',
-        hobbies: [],
-        eventId: ''
-      }
-      return events.length > 0 ? (
-        <Formik
-          initialValues={initialValues}
-          validationSchema={guestSchema}
-          onSubmit={this.onSubmit}
-          render={({ errors, status, touched, values }) => (
-            <Form>
-              {console.log('values: ', values)}
-              <Row>
-                <Col md={12}>
-                  <Select
-                    name="eventId"
-                    label="Event"
-                    defaultOptionLabel="Select an event"
-                    errors={errors.eventId}
-                    touched={touched.eventId}
-                  >
-                    {events.map(event => (
-                      <option value={event._id} key={event._id}>{event.name}: {event.description}</option>
-                    ))}
-                  </Select>
-                </Col>
-              </Row>
-              <Row>
-                <Col md={6}>
-                  <Input
-                    name="firstName"
-                    label="First Name"
-                    errors={errors.firstName}
-                    touched={touched.firstName}
-                  />
-                </Col>
-                <Col md={6}>
-                  <Input
-                    name="lastName"
-                    label="Last Name"
-                    errors={errors.lastName}
-                    touched={touched.lastName}
-                  />
-                </Col>
-              </Row>
-              <Row>
-                <Col md={6}>
-                  <Input
-                    name="email"
-                    label="E mail"
-                    type="email"
-                    errors={errors.email}
-                    touched={touched.email}
-                  />
-                </Col>
-                <Col md={6}>
-                  <Input
-                    name="dateOfBirth"
-                    label="Date of birth"
-                    type="date"
-                    max={todaysDate}
-                    errors={errors.dateOfBirth}
-                    touched={touched.dateOfBirth}
-                  />
-                </Col>
-              </Row>
-              <Row>
-                <Col md={6}>
-                  <Select
-                    name="gender"
-                    label="Gender"
-                    defaultOptionLabel="Select Gender"
-                    errors={errors.gender}
-                    touched={touched.gender}
-                  >
-                    {
-                      GENDERS.map((gender, i) => (
-                        <option value={gender} key={i}>{gender}</option>
-                      ))
-                    }
-                  </Select>
-                </Col>
-                <Col md={6}>
-                  <Checkbox
-                    name="hobbies"
-                    label="Hobbies"
-                    options={HOBBIES}
-                    values={values}
-                  />
-                </Col>
-              </Row>
-              <div className="form-group">
-                <button type="submit" className="btn btn-primary mr-2">Register</button>
-                <button type="reset" className="btn btn-secondary">Reset</button>
-              </div>
-            </Form>
-          )}
-        />
-      ) : (
-        <p className="text-center">No Events Available</p>
-      )
+  render () {
+    const { events } = this.props
+    const todaysDate = new Date().toISOString().split('T')[0]
+
+    const initialValues = {
+      // firstName: '',
+      // lastName: '',
+      // email: '',
+      // birthDate: '',
+      // gender: '',
+      // hobbies: [],
+      // eventId: ''
+      firstName: 'Nikolaos',
+      lastName: 'Lontorfos',
+      email: 'nick@trest.gr',
+      birthDate: '1986-03-31',
+      gender: 'Male',
+      hobbies: [
+        'Sports',
+        'Climbing'
+      ],
+      eventId: '5e3ed4a8f2fc6c257cc7c212'
     }
+    const onSubmit = () => {
+      console.log('props: ')
+    }
+    return events.length > 0 ? (
+      <Formik
+        initialValues={initialValues}
+        validationSchema={guestSchema}
+        onSubmit={onSubmit}
+      >
+        {({ errors,
+          touched,
+          values,
+          handleSubmit
+        }) => (
+          <Form onSubmit={handleSubmit}>
+            <Row>
+              <Col md={12}>
+                <Select
+                  name="eventId"
+                  label="Event"
+                  defaultOptionLabel="Select an event"
+                  errors={errors.eventId}
+                  touched={touched.eventId}
+                >
+                  {events.map(event => (
+                    <option value={event._id} key={event._id}>{event.name}: {event.description}</option>
+                  ))}
+                </Select>
+              </Col>
+            </Row>
+            <Row>
+              <Col md={6}>
+                <Input
+                  name="firstName"
+                  label="First Name"
+                  errors={errors.firstName}
+                  touched={touched.firstName}
+                />
+              </Col>
+              <Col md={6}>
+                <Input
+                  name="lastName"
+                  label="Last Name"
+                  errors={errors.lastName}
+                  touched={touched.lastName}
+                />
+              </Col>
+            </Row>
+            <Row>
+              <Col md={6}>
+                <Input
+                  name="email"
+                  label="E mail"
+                  type="email"
+                  errors={errors.email}
+                  touched={touched.email}
+                />
+              </Col>
+              <Col md={6}>
+                <Input
+                  name="birthDate"
+                  label="Date of birth"
+                  type="date"
+                  max={todaysDate}
+                  errors={errors.birthDate}
+                  touched={touched.birthDate}
+                />
+              </Col>
+            </Row>
+            <Row>
+              <Col md={6}>
+                <Select
+                  name="gender"
+                  label="Gender"
+                  defaultOptionLabel="Select Gender"
+                  errors={errors.gender}
+                  touched={touched.gender}
+                >
+                  {
+                    GENDERS.map((gender, i) => (
+                      <option value={gender} key={i}>{gender}</option>
+                    ))
+                  }
+                </Select>
+              </Col>
+              <Col md={6}>
+                <Checkbox
+                  name="hobbies"
+                  label="Hobbies"
+                  options={HOBBIES}
+                  values={values}
+                />
+              </Col>
+            </Row>
+            <div className="form-group">
+              <button type="submit" className="btn btn-primary mr-2">Register</button>
+              <button type="reset" className="btn btn-secondary">Reset</button>
+            </div>
+          </Form>
+        )}
+      </Formik>
+    ) : (
+      <p className="text-center">No Events Available</p>
+    )
+  }
 }
 
 export default EventForm
